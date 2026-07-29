c508c1b25cfcbe3fe3d36ba86bfc04cc0cda49d6
KingKlahador <klahador100@gmail.com>
2026-07-29 17:25:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const eventDate = new Date("2026-08-21T19:00:00").getTime();
+const eventDate = new Date("Ago 21, 2026 19:00:00").getTime();
 
-const timer = setInterval(() => {
+setInterval(() => {
 
     const now = new Date().getTime();
     const distance = eventDate - now;
 
-    if (distance <= 0) {
-        clearInterval(timer);
-
-        document.getElementById("days").textContent = 0;
-        document.getElementById("hours").textContent = 0;
-        document.getElementById("minutes").textContent = 0;
-        document.getElementById("seconds").textContent = 0;
-
-        return;
-    }
-
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000*60*60*24));
+    const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+    const seconds = Math.floor((distance%(1000*60))/1000);
 
     document.getElementById("days").textContent = days;
     document.getElementById("hours").textContent = hours;
     document.getElementById("minutes").textContent = minutes;
     document.getElementById("seconds").textContent = seconds;
 
-}, 1000);
+},1000);
